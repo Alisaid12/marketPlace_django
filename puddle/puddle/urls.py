293e489a21bf3ee08fa651716c76e19cb540ed0e
Alisaid12 <hahaali139@gmail.com>
2026-08-20 14:20:54 +0200

    adding items , dashboard

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('core.urls')),
     path('item/',include('item.urls')),
+    path('dashboard/',include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
 
