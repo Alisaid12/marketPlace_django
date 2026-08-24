b64aa56aaff14069c07106ee12d142f2a05d8504
Alisaid12 <hahaali139@gmail.com>
2026-08-24 18:04:05 +0200

    add searching

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm,EditItemForm
-from .models import Item
+from .models import Item,Category
 from django.contrib import messages
+from django.db.models import Q
+
+def items(request):
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    items = Item.objects.filter(is_sold=False)
+
+    categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category_id = category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request,'item/items.html',{
+        'items': items,
+        'query': query,
+        'categories':categories,
+        'category_id':int(category_id),
+    })
+
 
 def detail(request,pk):
     item = get_object_or_404(Item, pk=pk)
@@ -32,6 +53,7 @@ def new(request):
         'form':form,
         'title':"New Item"
     })
+
 @login_required
 def edit(request,pk):
     item = get_object_or_404(Item,pk=pk,created_by=request.user)
